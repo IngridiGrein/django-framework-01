a6578be9e91f58bf756a3a711521037943e7ce20
IngridiGrein <ingridigrein@gmail.com>
2020-04-28 21:52:25 -0300

    aula 28-04

--- cliente/urls.py
+++ cliente/urls.py
@@ -8,4 +8,9 @@ app_name = "cliente"
 urlpatterns = [
     path("", views.index, name="index"),
     path("hora/", views.hora, name="hora"),
+    path("dados/", views.dados, name="dados"),
+    path("cidade/", views.cidadeView, name="cidade"),
+    path("endereco/", views.enderecoView, name="endereco"),
+    path("cliente/", views.clienteView, name="cliente"),
+    path("cadastra", views.cadastra, name="cadastra")
 ]
--- cliente/views.py
+++ cliente/views.py
@@ -1,29 +1,65 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from datetime import datetime
+
+#Formuários
+from .forms import CidadeForm
+from .forms import EnderecoForm
+from .forms import ClienteForm
+
 # Create your views here.
 
 
 def index(request):
-    html= """<html>
-        <head>
-        </head>
-        <body>
-         <h1><marquee><font-size="20px">Hello World!</marquee></h1>
-        </body>    
-    </html>"""
-   
-
-    return HttpResponse(html)
+    return render(request, "cliente/index.html", {})
 
 def hora(request):
-    html= """<html>
-        <head>
-        </head>
-        <body>
-         <h1>%s</h1>
-        </body>    
-    </html>"""% datetime.now()
 
-    return HttpResponse(html)
+    context = {"hora":datetime.now()}
+    return render(request, "cliente/hora.html", context)
+
+def dados(request):
+    nome = "Ingridi"
+    dtNascimento = "08/05/1993"
+    dog = "Penny"
+    cidade = "Curitiba"
+    cpf = "10"
+    rg = "12"
+
+    context = {
+        "nome":nome,
+        "dtNascimento":dtNascimento,
+        "dog":dog,
+        "cidade":cidade,
+        "cpf":cpf,
+        "rg":rg,
+    }
+    return render(request, "cliente/dados.html", context)
+
+def cidadeView(request):
+
+    form = CidadeForm()
+
+    context = {"form":form}
+
+    return render(request, "cliente/cidade.html", context)
+
+def enderecoView(request):
+
+    form = EnderecoForm()
+
+    context = {"form":form}
+
+    return render(request, "cliente/endereco.html", context)    
+
+def clienteView(request):
+
+    form = ClienteForm()
+
+    context = {"form":form}
+
+    return render(request, "cliente/cliente.html", context)      
+
+def cadastra(request):
+    pass      
